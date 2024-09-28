bedcfcfd7357c72b7a258f077e91c223ba56c2fe
gdeherdt <gdeherdt@purdue.edu>
2024-09-27 21:48:37 -0400

    Update run.js

--- out/run.js
+++ out/run.js
@@ -1,29 +1,29 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 //const { execSync } = require('child_process');
-const node_child_process_1 = require("node:child_process");
+import { execSync } from "node:child_process";
 //execSync(Command, { stdio: 'inherit' });
-function runCommand(file) {
-    const command = `node ${file}`;
-    try {
-        (0, node_child_process_1.execSync)(command, { stdio: 'inherit' });
-        console.log(`Successfully ran ${file}`);
-    }
-    catch (error) {
-        console.error(`Failed to run ${file}`);
+function runCommand(fname:string){
+    let file:string = __dirname+'//'+fname;
+    const command = `node ${file}`
+    try{
+    execSync(command, { stdio: 'inherit' });
+    console.log(`Successfully ran ${file}`);
+    }catch (error) {
+      console.error(`Failed to run ${file}`)  ;
     }
 }
-function runMaster(file, urlFile) {
-    const command = `node ${file} ${urlFile}`;
-    try {
-        (0, node_child_process_1.execSync)(command, { stdio: 'inherit' });
+    function runMaster(fname:string, urlFile:string){
+        let file:string = __dirname+`//`+fname;
+        const command = `node ${file} ${urlFile}`
+        console.log(`running subprogram ${file} to test ${urlFile}`);
+        try{
+        execSync(command, { stdio: 'inherit' });
         console.log(`Successfully ran ${urlFile}`);
-    }
-    catch (error) {
-        console.error(`Failed to run ${urlFile}`);
-    }
-}
-var n = 0;
+        }catch (error) {
+          console.error(`Failed to run ${urlFile}`) ; 
+        }
+    
+ }
+var n = 0; //root command loc
 //while(n<3){
 //n++;
 //        console.log(`\n argument ${n} is ${process.argv[n]}`);
@@ -31,20 +31,28 @@ var n = 0;
 //process.exit(1);
 //}
 //}
-n = 2;
-const commandString = process.argv[n];
-if (commandString == null) {
-    console.error(`Failed to run`);
-    process.exit(1);
+try{
+while (process.argv[n] != __filename){
+    n++;
 }
-if (commandString == 'install') {
+ n=n+1
+} catch(error){
+    console.error(`Failed to run program`);
+}
+const commandString:string = process.argv[n];
+if (commandString == null){
+    console.error(`Failed to run, invalid arguments`);
+    process.exit(0) ;
+}
+if (commandString == 'install'){
     runCommand('Install.js');
 }
-else if (commandString == 'test') {
+else
+if (commandString == 'test'){
     runCommand('TestRun.js');
 }
 else {
-    const url = process.argv[n];
-    runMaster('Master.js', url);
+    
+    const url:string = process.argv[n];
+    runMaster('Master.js',url);
 }
-//# sourceMappingURL=run.js.map
