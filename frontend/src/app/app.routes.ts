f9a37edeb64d3a03e28999ddc355a9e735f2fad6
WillKissel02 <wkissel@purdue.edu>
2024-12-08 16:18:08 -0500

    Adjust frontend layout

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { MainComponent } from './main/main.component';
-
+import { PackageUploadComponent } from './package-upload/package-upload.component';
 export const routes: Routes = [
   { path: '', component: MainComponent }, // Default route
   { path: 'about', component: AboutComponent },
+  { path: 'upload', component: PackageUploadComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' }, // Wildcard route
 ];
