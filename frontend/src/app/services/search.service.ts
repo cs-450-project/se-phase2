05042d0e893fc896cac357203376999c888d1386
WillKissel02 <wkissel@purdue.edu>
2024-12-08 19:28:15 -0500

    fix api url

--- frontend/src/app/services/search.service.ts
+++ frontend/src/app/services/search.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
-  private apiUrl = 'http://localhost:3000/packages';
+  private apiUrl = `${environment.apiUrl}/packages`;
 
   constructor(private http: HttpClient) {}
 
